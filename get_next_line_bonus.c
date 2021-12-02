7dc110d6530be15c27b55f7686b8b69e8af9f9cf
MyNameIsTrez <welfje@gmail.com>
2021-12-02 16:15:32 +0100

    Add bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,19 +6,102 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/30 14:03:45 by sbos          #+#    #+#                 */
-/*   Updated: 2021/11/30 14:03:46 by sbos          ########   odam.nl         */
+/*   Updated: 2021/12/02 16:13:00 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Excerpt from "man 2 select":
-The default size FD_SETSIZE (currently 1024) is somewhat smaller
-than the current kernel limit to the number of open files.
-However, in order to accommodate programs which might potentially use
-a larger number of open files with select, it is possible to increase this size
-within a program by providing a larger definition of FD_SETSIZE
-before the inclusion of <sys/types.h>."
+#include "get_next_line_bonus.h"
+
+#include <unistd.h> // read
+#include <stdlib.h> // malloc
+
+static char	*gnl_malloc_line(t_buffer_list *lst)
+{
+	size_t	len;
+	char	*line;
+	char	*nl;
+
+	if (lst->size == 0)
+		return (NULL);
+	len = 0;
+	while (lst->next != NULL)
+	{
+		len += (size_t)(lst->size - lst->start);
+		lst = lst->next;
+	}
+	nl = gnl_find_newline(lst);
+	if (nl == NULL)
+		len += (size_t)lst->size;
+	else
+		len += (size_t)(nl - (lst->buf + lst->start)) + 1;
+	line = malloc(len + 1);
+	if (line == NULL)
+		return (NULL);
+	if (nl != NULL)
+		line[len - 1] = '\n';
+	line[len] = '\0';
+	return (line);
+}
+
+// if (j != (*lst)->size && j + 1 < (*lst)->size)
+// if (j < (*lst)->size && j + 1 < (*lst)->size)
+static char	*gnl_create_line(t_buffer_list **lst)
+{
+	char			*line;
+	ssize_t			i;
+	ssize_t			j;
+
+	line = gnl_malloc_line(*lst);
+	if (line == NULL)
+		return (gnl_lst_clear(lst));
+	i = 0;
+	while (*lst != NULL)
+	{
+		j = (*lst)->start;
+		while (j < (*lst)->size && (*lst)->buf[j] != '\n')
+		{
+			line[i] = (*lst)->buf[j];
+			i++;
+			j++;
+		}
+		if (j + 1 < (*lst)->size)
+		{
+			(*lst)->start = j + 1;
+			return (line);
+		}
+		gnl_next(lst);
+	}
+	return (line);
+}
+
+/**
+ * @brief Gets the next line from @p fd, including the newline.
+ * @param fd The file descriptor to read from.
+ * @return The next line from @p fd.
 */
-#ifndef OPEN_MAX
-# define OPEN_MAX FD_SETSIZE
-#endif
+char	*get_next_line(int fd)
+{
+	static t_buffer_list	*lst_fds[OPEN_MAX];
+	t_buffer_list			**lst;
+	t_buffer_list			*cur;
+
+	if (fd < 0 || fd >= OPEN_MAX || BUFFER_SIZE < 1)
+		return (NULL);
+	lst = &lst_fds[fd];
+	if (gnl_find_newline(*lst))
+		return (gnl_create_line(lst));
+	cur = gnl_lst_new_back(lst);
+	if (cur == NULL)
+		return (NULL);
+	cur->size = read(fd, cur->buf, BUFFER_SIZE);
+	while (gnl_find_newline(cur) == NULL && cur->size > 0)
+	{
+		cur = gnl_lst_new_back(lst);
+		if (cur == NULL)
+			return (gnl_lst_clear(lst));
+		cur->size = read(fd, cur->buf, BUFFER_SIZE);
+	}
+	if (cur->size < 0)
+		return (gnl_lst_clear(lst));
+	return (gnl_create_line(lst));
+}
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,72 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/30 14:03:50 by sbos          #+#    #+#                 */
-/*   Updated: 2021/11/30 14:03:51 by sbos          ########   odam.nl         */
+/*   Updated: 2021/12/02 15:54:40 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line_bonus.h"
+
+#include <stdlib.h> // malloc
+
+static t_buffer_list	*gnl_lst_new(void)
+{
+	t_buffer_list	*new;
+
+	new = malloc(sizeof(t_buffer_list));
+	if (new == NULL)
+		return (NULL);
+	new->start = 0;
+	new->next = NULL;
+	return (new);
+}
+
+t_buffer_list	*gnl_lst_new_back(t_buffer_list **lst)
+{
+	t_buffer_list	*new;
+
+	new = gnl_lst_new();
+	if (new == NULL)
+		return (NULL);
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		while ((*lst)->next != NULL)
+			lst = &((*lst)->next);
+		(*lst)->next = new;
+	}
+	return (new);
+}
+
+void	gnl_next(t_buffer_list **lst)
+{
+	t_buffer_list	*next;
+
+	next = (*lst)->next;
+	free(*lst);
+	*lst = next;
+}
+
+char	*gnl_find_newline(t_buffer_list *lst)
+{
+	ssize_t	i;
+
+	if (lst == NULL)
+		return (NULL);
+	i = lst->start;
+	while (i < lst->size)
+	{
+		if (lst->buf[i] == '\n')
+			return (&lst->buf[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*gnl_lst_clear(t_buffer_list **lst)
+{
+	while (*lst != NULL)
+		gnl_next(lst);
+	return (NULL);
+}
